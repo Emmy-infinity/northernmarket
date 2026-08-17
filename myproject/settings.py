85924afc3728d01d2e4e8099bbf5b38f9ecb4f05
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-08-18 02:12:04 +0300

    Change Whitenoise storage backend for static files

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,7 +131,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",  # ← Changed
     },
 }
 
