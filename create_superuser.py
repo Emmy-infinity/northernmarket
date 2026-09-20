2f7c4e0bd70b31d9961bb0f25375a8afa1c6549b
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-09-21 02:24:10 +0300

    Update default superuser password

--- create_superuser.py
+++ create_superuser.py
@@ -12,7 +12,7 @@ User = get_user_model()
 # 2. Get credentials from environment variables
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'SecretPassword123')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'SecretPassword1234')
 
 # 3. Create user if they do not exist
 if not User.objects.filter(username=username).exists():
